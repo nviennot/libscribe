3401e765939e7a9a6ec62c3d1a4d776d657b16e8
Nicolas Viennot <nicolas@viennot.biz>
2011-03-07 06:44:42 -0500

    ABI Update (Removed the Cred resource type)

--- src/debug.c
+++ src/debug.c
@@ -360,7 +360,6 @@ static const char *get_res_raw_type_str(int type)
 		case SCRIBE_RES_TYPE_PID: return "pid";
 		case SCRIBE_RES_TYPE_FUTEX: return "futex";
 		case SCRIBE_RES_TYPE_IPC: return "ipc";
-		case SCRIBE_RES_TYPE_CRED: return "credential";
 		case SCRIBE_RES_TYPE_PTRACE: return "ptrace";
 		default: return "unknown type";
 	}
